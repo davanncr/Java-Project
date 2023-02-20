08a2c990b54347d3b602b00b4ff2e90f1e31f59c
Davann CR <davannsmart9@gmail.com>
2023-02-20 17:40:06 +0700

    1.0.2

--- Final_OOP/src/Feature/Hire.java
+++ Final_OOP/src/Feature/Hire.java
@@ -1,9 +1,12 @@
 package Feature;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Hire {
     public static JPanel getPanel(){
+        JPanel panel = new JPanel();
+        panel.setSize(400,800);
         return new JPanel();
     }
 }
